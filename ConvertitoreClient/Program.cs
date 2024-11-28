a1362a5bf17802df16fa68b22fa0a0c92fbd543c
Davide Luppoli <davide@davideluppoli.it>
2024-11-28 09:07:43 +0100

    Add project files.

---
+++ ConvertitoreClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConvertitoreClient.ConvertitoreService;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertitoreClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceClient service = new ServiceClient();
+            string scelta = "";
+
+            while(scelta!="9")
+            {
+                Console.WriteLine("1 - Conversione valute");
+                Console.WriteLine("2 - Stampa tassi di conversione");
+                Console.WriteLine();
+                Console.WriteLine("9 - Uscita");
+
+                scelta = Console.ReadLine();
+
+                switch(scelta)
+                {
+                    case "1":
+
+
+                        foreach(valute valuta in Enum.GetValues(typeof(valute)))
+                        {
+                            Console.WriteLine( ((int)valuta).ToString() + " - "+valuta.ToString());
+                        }
+                        Console.Write("Importo: ");
+                        float importo = float.Parse(Console.ReadLine());
+                        Console.Write("Valuta di partenza: ");
+                        int da = int.Parse(Console.ReadLine());
+                        Console.Write("Valuta di destinazione: ");
+                        int a = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine( service.ConvertiValuta(importo,(valute)da,(valute)a));
+                        break;
+                    case "2":
+                        foreach(string tasso in service.TassiConversione() )
+                            Console.WriteLine(tasso);
+                        break;
+                    case "9":
+                        break;
+                    default:
+                        Console.WriteLine("Scelta errata");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConvertitoreService/App_Code/Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    Dictionary<string, float> tassiConversione;
+
+    public float ConvertiValuta(float importo, valute da, valute a)
+    {
+        if (da == a) return importo;
+
+        float importoEUR = importo / tassiConversione.ElementAt((int)da).Value;
+
+        return importoEUR * tassiConversione.ElementAt((int)a).Value;
+    }
+
+    public string[] TassiConversione()
+    {
+        return tassiConversione
+            .Where(w=>w.Key != "EUR")
+            .Select(s => "1 EUR = " + s.Value + " " + s.Key)
+            .ToArray();
+    }
+
+    public Service()
+    {
+        tassiConversione = new Dictionary<string, float>();
+        tassiConversione.Add("EUR", 1f);
+        tassiConversione.Add("ITL", 1936.27f);
+        tassiConversione.Add("DEM", 1.95583f);
+        tassiConversione.Add("FRF", 6.55957f);
+    }
+}
